0ce7c192ed28823c0c7a7dbfec8e19d50dd4ae81
NCK36 <navinchandan0@gmail.com>
2017-04-21 12:24:05 +0530

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -13,7 +13,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 #Reading and loading the data
 destinations = pd.read_csv("destinations.csv")
-test = pd.read_csv("test.csv", nrows=100000)
+test = pd.read_csv("test.csv", nrows=250000)
 train = pd.read_csv("train.csv", nrows = 5000000)
 train.head()
 
